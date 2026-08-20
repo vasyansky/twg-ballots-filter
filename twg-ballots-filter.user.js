80a51b6c317a39f8101f00a10fed44ce5d32f9e1
vasya <vasyaisvasya@proton.me>
2026-08-20 13:06:43 +0300

    add run-at to userscript for running after page loaded

--- twg-ballots-filter.user.js
+++ twg-ballots-filter.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name        Ballots filter for turbo.jam.gd
 // @match       https://turbo.jam.gd/ru/v0/results?view=ballots
+// @run-at      document-idle
 // @icon        https://static-cdn-tmp.jam.gd/-Wh543e4uqh5swN7uHNYPVKzKEN-TfO4zUni4_7qlc0/f:webp/rs::64:64/czM6Ly9qYW1nZC1zdGF0aWMvYWY4YjRmMTMtZDkxMS00ZDZiLWFkNWItNmRmZTM4YTY0YmUwL2ZhdjY0LndlYnA
 // @grant       none
 // @version     1.0
